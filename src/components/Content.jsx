649066b6b803693d9f5a760b5b117c1ab8c01ff8
Rick-torrellas <75484605+Rick-torrellas@users.noreply.github.com>
2021-08-22 10:48:54 -0400

    fix: detalles

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Title from './../partials/Title';
-import dataRaw from './../data/data';
-import Icons from './../partials/Icons';
+/* import dataRaw from './../data/data';
+import Icons from './../partials/Icons'; */
 
 export default function Content() {
     return (
